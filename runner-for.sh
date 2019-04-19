0e7eb653793c47a1c826dbd537159af2ef21bc71
Petr Nymsa <nymsa.petr@outlook.cz>
2019-04-19 21:28:21 +0200

    make repair

--- runner-for.sh
+++ runner-for.sh
@@ -4,7 +4,7 @@ for file in ./data/*
 do
     if [[ -f $file ]]; then
 	result=$((time ./a.out $file) |& grep real | awk '{print $2}')
-	echo "$file: $result" >> result-tasks.txt
+	echo "$file: $result" >> result-for.txt
     fi
 done
 
