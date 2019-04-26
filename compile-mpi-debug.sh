0986e367a554d89909fafe25446e05b88d6ef222
Petr Nymsa <nymsa.petr@outlook.cz>
2019-04-26 22:39:44 +0200

    compile mpi script debug mode

---
+++ compile-mpi-debug.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export OMPI_CXX=g++
+echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+mpic++ main.cpp src/array_map.h src/map_info.h -Wall -pedantic -O3 -Wextra -g -D DEBUG -fopenmp -o mpi.out
